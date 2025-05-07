7308d855c64e622fcbf3368f93df227416ed6472
Katarzyna Pietrzyk <katarzynapietrzyk2806@gmail.com>
2025-05-07 16:43:02 +0200

    date type changed and id fixed

--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -4,14 +4,14 @@ from datetime import datetime
 from .constants import CATEGORY_COLORS
 
 class EventSerializer(serializers.ModelSerializer):
-    color = serializers.SerializerMethodField()
+    id = serializers.SerializerMethodField()
     start = serializers.SerializerMethodField()
     end = serializers.SerializerMethodField()
     color = serializers.SerializerMethodField()
 
     class Meta:
         model = Event
-        fields = ['id','title', 'description', 'start_date', 'start_time', 'end_date', 'end_time', 'category', 'repeat_type'] 
+        fields = ['id','title', 'description', 'start_date', 'end_date', 'category', 'repeat_type']  #'start_time', 'end_time',
 
     def get_start(self, obj):
         return datetime.combine(obj.date, obj.time)
